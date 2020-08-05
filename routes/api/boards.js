1c2e1c1faad6bab1ecb19e70a0893c4c1635ed60
Gorham <kevin.gorham@gmail.com>
2020-08-05 14:10:55 -0700

    working on boardsController.js

--- routes/api/boards.js
+++ routes/api/boards.js
@@ -1,70 +1,52 @@
 const router = require("express").Router();
-const booksController = require("../../controllers/booksController");
+const boardsController = require("../../controllers/boardsController");
 
-// Matches with "/api/books"
-router.route("/")
-  .get(booksController.findAll)
-  .post(booksController.create);
+// Matches with "/api/boards"
+
+// router.route("/")
+//   .get(booksController.findAll)
+//   .post(booksController.create);
 
 // Matches with "/api/books/:id"
+// router
+//   .route("/:id")
+//   .get(booksController.findById)
+//   // .put(booksController.update)
+//   .delete(booksController.remove);
+
+// Route for associating a movie with a board
+router
+  .route("/movie")
+  .post(boardsController.movie)
+
+// Route for getting a board & associate data by Id
 router
   .route("/:id")
-  .get(booksController.findById)
-  // .put(booksController.update)
-  .delete(booksController.remove);
-
-module.exports = router;
+  .get(boardsController.findById)
 
 
 
-
-  // Route for associating a movie with a board
-  app.post("/api/boards_movies/", function(req, res) {
-    db.Boards_Movies.create({
-      MovieId: req.body.MovieId,
-      BoardId: req.body.BoardId,
-    })
-      .then((dbRecord) => {
-        res.json(dbRecord);
-      })
-      .catch(function(err) {
-        console.log(err);
-        res.status(409).json(err);
-      });
-  });
-
-  // add vote to a board_movie record
-  app.put("/api/boards_movies/", function(req, res) {
-    db.Boards_Movies.update(
-      {
-        numVotes: req.body.votes,
+// add vote to a board_movie record
+app.put("/api/boards_movies/", function(req, res) {
+  db.Boards_Movies.update(
+    {
+      numVotes: req.body.votes,
+    },
+    {
+      where: {
+        MovieId: req.body.movieId,
+        BoardId: req.body.boardId,
       },
-      {
-        where: {
-          MovieId: req.body.movieId,
-          BoardId: req.body.boardId,
-        },
-      }
+    }
     )
-      .then((dbRecord) => {
-        res.json(dbRecord);
-      })
-      .catch((err) => {
-        res.status(500).json(err);
-      });
-  });
-
-    // Route for getting a group's active board's movies, and numVotes for each
-    app.get("/api/group/movies/:boardId", function(req, res) {
-      // query board, including the associated movies
-      db.Board.findOne({
-        where: {
-          id: req.params.boardId,
-        },
-        include: {
-          model: db.Movie
-        },
-      }).then((dbBoard_Movies) => {
-        res.json(dbBoard_Movies);
-      });
+    .then((dbRecord) => {
+      res.json(dbRecord);
+    })
+    .catch((err) => {
+      res.status(500).json(err);
     });
+  });
+  
+ 
+  
+  module.exports = router;
