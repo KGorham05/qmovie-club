3ea2581cd7e08e45e524b42032725bc8a680e61b
Gorham <kevin.gorham@gmail.com>
2020-08-05 15:54:33 -0700

    users routes/controller complete

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,13 +1,15 @@
-const router = require("express").Router();
+const router      = require("express").Router();
 const boardRoutes = require("./boards");
 const groupRoutes = require("./groups");
+const userRoutes  = require("./users");
 const movieRoutes = require("./movies");
-const userRoutes = require("./users");
+const authRoutes  = require("./auth");
 
 // board routes
 router.use("/boards", boardRoutes);
 router.use("/groups", groupRoutes);
-router.use("/users", userRoutes);
+router.use("/users" , userRoutes);
 router.use("/movies", movieRoutes);
+router.use("/auth"  , authRoutes);
 
 module.exports = router;
--- routes/api/users.js
+++ routes/api/users.js
@@ -1,41 +1,14 @@
-const Sequelize = require("sequelize");
-const Op = Sequelize.Op;
+const router = require("express").Router();
+const usersController = require("../../controllers/usersController");
 
-// get user data
-app.get("/api/user", function(req, res) {
-  if (!req.user) {
-    console.log("No user logged in");
-    res.json({});
-  } else {
-    req.user.email
-      ? res.json({
-          email: req.user.email,
-          id: req.user.id,
-        })
-      : res.json({
-          firstName: req.user[0].firstName,
-          lastName: req.user[0].lastName,
-        });
-  }
-});
+// These routes match on /api/users
 
-// Route for resetting the numVotes for all users, that will run on a cron job
-app.put("/api/users", function(req, res) {
-  db.Users_Groups.update(
-    {
-      numVotes: 3,
-    },
-    {
-      where: {
-        numVotes: {
-          [Op.lte]: [3],
-        },
-      },
-    }
-  ).then((dbUsers) => {
-    console.log("Votes Reset");
-    res.json({
-      reset: true,
-    });
-  });
-});
+router
+  .route("/")
+  .get(usersController.findOne)
+
+router
+  .route("/resetVotes")
+  .put(usersController.resetVotes)
+
+
