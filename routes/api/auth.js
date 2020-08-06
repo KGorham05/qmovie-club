25c040e9be365972496295e524b79576e3d208a1
Gorham <kevin.gorham@gmail.com>
2020-08-05 21:09:51 -0700

    removed extra white space

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,15 +1,9 @@
 const router = require("express").Router();
 const authController = require("../../controllers/authController");
 const passport = require("../../config/passport");
-// Routes will match on /api/auth
 
 router.post("/signup", authController.signUp, passport.authenticate(["local", "google"]), authController.login)
- 
-
-router.post("/login", passport.authenticate(["local", "google"]), authController.login)
-
-router
-  .route("/logout")
-  .get(authController.logout)
+router.post("/login" , passport.authenticate(["local", "google"]), authController.login)
+router.get("/logout" , authController.logout)
 
 module.exports = router;
